33640dbd34a9479663589c991f20133aa6d3ecf1
RaelCappra <rael.cappra@gmail.com>
2015-03-27 16:04:27 -0300

    adicionado metodo getByLogin no Cliente DAO

--- Projeto RestauranteMQ/src/persistencia/ClienteDAO.java
+++ Projeto RestauranteMQ/src/persistencia/ClienteDAO.java
@@ -1,5 +1,3 @@
-
-
 package persistencia;
 
 import java.sql.PreparedStatement;
@@ -11,18 +9,17 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 import model.Cliente;
 
-public class ClienteDAO implements Dao<Cliente, Long>{
+public class ClienteDAO implements Dao<Cliente, Long> {
 
     @Override
     public void save(Cliente entity) {
 
         try {
             ConexaoPostgreSQL conn = new ConexaoPostgreSQL("localhost", "postgres", "postgres", "postgres");
-            
-            String sql = "insert into cliente (cpf, nome, endereco, telefone, login, senha)  "+
-                         "values(?,?,?,?,?,?)";
-            
-            
+
+            String sql = "insert into cliente (cpf, nome, endereco, telefone, login, senha)  "
+                    + "values(?,?,?,?,?,?)";
+
             try (PreparedStatement ps = conn.getConnection().prepareStatement(sql)) {
                 ps.setString(1, entity.getCpf());
                 ps.setString(2, entity.getNome());
@@ -30,86 +27,110 @@ public class ClienteDAO implements Dao<Cliente, Long>{
                 ps.setString(4, entity.getTelefone());
                 ps.setString(5, entity.getLogin());
                 ps.setString(6, entity.getSenha());
-                
+
                 ps.execute();
-                
+
                 conn.fechar();
             }
-            
-            
-            
+
         } catch (Exception ex) {
             Logger.getLogger(ClienteDAO.class.getName()).log(Level.SEVERE, null, ex);
-        } 
-        
-        
+        }
+
     }
 
-    
     @Override
     public void delete(Long id) {
 
-        
-         try {
+        try {
             ConexaoPostgreSQL conn = new ConexaoPostgreSQL("localhost", "postgres", "postgres", "postgres");
-            
-            String sql = "delete from cliente where cliente.id = "+id;
-            
-            
+
+            String sql = "delete from cliente where cliente.id = " + id;
+
             try (PreparedStatement ps = conn.getConnection().prepareStatement(sql)) {
-                
-                
+
                 ps.execute();
-                
+
                 conn.fechar();
             }
-            
-            
-            
+
         } catch (Exception ex) {
             Logger.getLogger(ClienteDAO.class.getName()).log(Level.SEVERE, null, ex);
-        } 
- 
+        }
+
     }
 
     @Override
     public List<Cliente> listAll() {
-        
+
         List<Cliente> lista = new ArrayList<>();
-        
-         try {
+
+        try {
             ConexaoPostgreSQL conn = new ConexaoPostgreSQL("localhost", "postgres", "postgres", "postgres");
-            
+
             String sql = "select * from cliente";
-            
-            
+
             try (PreparedStatement ps = conn.getConnection().prepareStatement(sql)) {
-                
+
                 ResultSet rs = ps.executeQuery();
-                
-                while(rs.next()){
+
+                while (rs.next()) {
                     Cliente c = new Cliente();
-                    
+
                     c.setId(rs.getLong("id"));
                     c.setCpf(rs.getString("cpf"));
                     c.setEndereco(rs.getString("endereco"));
-                    c.setTelefone( rs.getString("telefone"));
-                    c.setLogin( rs.getString("login"));
-                    c.setSenha( rs.getString("senha"));
-                    c.setNome( rs.getString("nome"));
+                    c.setTelefone(rs.getString("telefone"));
+                    c.setLogin(rs.getString("login"));
+                    c.setSenha(rs.getString("senha"));
+                    c.setNome(rs.getString("nome"));
                     lista.add(c);
-                }      
+                }
                 conn.fechar();
             }
-            
-            
-            
+
         } catch (Exception ex) {
             Logger.getLogger(ClienteDAO.class.getName()).log(Level.SEVERE, null, ex);
-        } 
+        }
         return lista;
-        
+
     }
-    
-    
+
+    public Cliente getByLogin(String login) {
+        try {
+            ConexaoPostgreSQL conn = new ConexaoPostgreSQL("localhost", "postgres", "postgres", "postgres");
+
+            String sql = "select * from cliente where login = (?)";
+
+            try (PreparedStatement ps = conn.getConnection().prepareStatement(sql)) {
+                ps.setString(1, login);
+                ResultSet rs = ps.executeQuery();
+
+                if (rs.next()) {
+                    Cliente c = new Cliente();
+
+                    c.setId(rs.getLong("id"));
+                    c.setCpf(rs.getString("cpf"));
+                    c.setEndereco(rs.getString("endereco"));
+                    c.setTelefone(rs.getString("telefone"));
+                    c.setLogin(rs.getString("login"));
+                    c.setSenha(rs.getString("senha"));
+                    c.setNome(rs.getString("nome"));
+                    
+                    return c;
+
+                } else {
+                    throw new Exception("Não há cliente com o login " + login);
+                }
+
+            } finally {
+                conn.fechar();
+            }
+
+        } catch (Exception ex) {
+            Logger.getLogger(ClienteDAO.class.getName()).log(Level.SEVERE, null, ex);
+            return null;
+        }
+
+    }
 }
