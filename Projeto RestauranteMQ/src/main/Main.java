e86c09911065a626f9d66c71ff9df922f648d632
RaelCappra <rael.cappra@gmail.com>
2015-04-04 20:43:37 -0300

    Apresentacao: implementado metodo loginCliente(), criadas assinaturas dos metodos menuFuncionario() e menuCliente()

--- Projeto RestauranteMQ/src/main/Main.java
+++ Projeto RestauranteMQ/src/main/Main.java
@@ -1,21 +1,25 @@
 package main;
 
 import java.util.InputMismatchException;
-import java.util.List;
 import java.util.Scanner;
 import model.Cliente;
-import persistencia.ClienteDAO;
+import model.Funcionario;
 
 public class Main {
 
     private final static Scanner scanner = new Scanner(System.in);
 
+    static {
+        //scanner.useDelimiter(System.lineSeparator());
+    }
     private final static int SAIR = 0;
     private final static int CADASTRO_CLIENTE = 1;
     private final static int LOGIN_CLIENTE = 2;
     private final static int LOGIN_FUNCIONARIO = 3;
 
     public static void main(String[] args) {
+        System.out.println("BEM VINDO AO RESTAURANTEMQ\n");
+
         int opcao;
 
         while ((opcao = menuInicial()) != SAIR) {
@@ -38,7 +42,6 @@ public class Main {
     }
 
     private static int menuInicial() {
-        System.out.println("BEM VINDO AO RESTAURANTEMQ\n");
         System.out.println("Escolha sua opção:");
         System.out.println(CADASTRO_CLIENTE + " - Cadastrar cliente");
         System.out.println(LOGIN_CLIENTE + " - Login como cliente");
@@ -62,6 +65,29 @@ public class Main {
 
     private static void loginCliente() {
         System.out.println("LOGIN DE CLIENTE\n");
+        System.out.print("Nome de usuario:");
+        String username = scanner.next();
+        System.out.print("Senha:");
+        String senha = scanner.next();
+
+        Cliente cliente = new Cliente();
+        boolean login = cliente.login(username, senha);
+        if (login) {
+            System.out.println("Bem vindo, " + cliente.getNome() + "!");
+            menuCliente(cliente);
+        } else {
+            System.out.println("Nome de usuario e/ou senha incorretos");
+        }
     }
 
+    private static void menuCliente(Cliente cliente) {
+        //TODO: implementar    
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    private static void menuFuncionario(Funcionario funcionario) {
+        //TODO: implementar    
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+
+    }
 }
