bd04cec1026cc20223646c81411eff4a08085224
Razue <lucas.viana.rs@hotmail.com>
2015-04-04 22:16:38 -0300

    PedidoDAO pronto.

--- Projeto RestauranteMQ/src/persistencia/PedidoDAO.java
+++ Projeto RestauranteMQ/src/persistencia/PedidoDAO.java
@@ -4,7 +4,14 @@
  */
 package persistencia;
 
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import model.Cliente;
 import model.Pedido;
 
 /**
@@ -15,22 +22,145 @@ public class PedidoDAO implements Dao<Pedido, Long>{
 
     @Override
     public void save(Pedido entity) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        
+        ConexaoPostgreSQL conn = null;
+        try {
+            conn = new ConexaoPostgreSQL("localhost", "postgres", "postgres", "postgres");
+
+            String sql = "insert into pedido (data, cliente, endereco, observacoes)  "
+                    + "values(?,?,?,?)";
+
+            try (PreparedStatement ps = conn.getConnection().prepareStatement(sql)) {
+                ps.setDate(1, new java.sql.Date(entity.getDataEHora().getTimeInMillis()));
+                ps.setLong(2, entity.getCliente().getId());
+                ps.setString(3, entity.getEndereco());
+                ps.setString(4, entity.getObservações());
+               
+                ps.execute();
+
+                conn.fechar();
+            }
+
+        } catch (Exception ex) {
+            Logger.getLogger(ClienteDAO.class.getName()).log(Level.SEVERE, null, ex);
+        }  finally {
+            if (conn != null) {
+                conn.fechar();
+            }
+        }
     }
 
     @Override
     public void delete(Long id) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        ConexaoPostgreSQL conn = null;
+        try {
+            conn = new ConexaoPostgreSQL("localhost", "postgres", "postgres", "postgres");
+
+            String sql = "delete from pedido where id = ?";
+
+            try (PreparedStatement ps = conn.getConnection().prepareStatement(sql)) {
+                ps.setLong(1, id);
+                ps.execute();
+
+                conn.fechar();
+            }
+
+        } catch (Exception ex) {
+            Logger.getLogger(ClienteDAO.class.getName()).log(Level.SEVERE, null, ex);
+
+        } finally {
+            if (conn != null) {
+                conn.fechar();
+            }
+        }
     }
 
     @Override
     public List<Pedido> listAll() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+         List<Pedido> lista = new ArrayList<>();
+
+        ConexaoPostgreSQL conn = null;
+        try {
+            conn = new ConexaoPostgreSQL("localhost", "postgres", "postgres", "postgres");
+
+            String sql = "select * from pedido";
+
+            try (PreparedStatement ps = conn.getConnection().prepareStatement(sql)) {
+
+                ResultSet rs = ps.executeQuery();
+
+                while (rs.next()) {
+                    Pedido c = new Pedido();
+
+                    c.setId(rs.getLong("id"));
+                    Calendar data = Calendar.getInstance(); 
+                    data.setTime(rs.getDate("data"));
+                    c.setDataEHora(data);
+                    c.setEndereco(rs.getString("endereco"));
+                    c.setObservações(rs.getString("observacoes"));
+                    ClienteDAO clienteDao = new ClienteDAO();
+                    c.setCliente(clienteDao.getById(rs.getLong("cliente")));
+
+                    lista.add(c);
+
+                }
+
+            }
+            conn.fechar();
+
+        } catch (Exception ex) {
+            Logger.getLogger(ClienteDAO.class.getName()).log(Level.SEVERE, null, ex);
+
+        } finally {
+            if (conn != null) {
+                conn.fechar();
+            }
+        }
+
+        return lista;
     }
 
     @Override
     public Pedido getById(Long pk) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        Pedido c = new Pedido();
+        ConexaoPostgreSQL conn = null;
+        try {
+            conn = new ConexaoPostgreSQL("localhost", "postgres", "postgres", "postgres");
+
+            String sql = "select * from pedido where id = ?";
+
+            try (PreparedStatement ps = conn.getConnection().prepareStatement(sql)) {
+
+                ps.setLong(1, pk);
+
+                ResultSet rs = ps.executeQuery();
+                if(rs.next()){
+
+                c = new Pedido();
+
+                    c.setId(rs.getLong("id"));
+                    Calendar data = Calendar.getInstance(); 
+                    data.setTime(rs.getDate("data"));
+                    c.setDataEHora(data);
+                    c.setEndereco(rs.getString("endereco"));
+                    c.setObservações(rs.getString("observacoes"));
+                    ClienteDAO clienteDao = new ClienteDAO();
+                    c.setCliente(clienteDao.getById(rs.getLong("cliente")));
+                    
+                } else {
+                    throw new Exception("Não há pedido com o id " + pk);
+                }
+
+            }
+
+        } catch (Exception ex) {
+            Logger.getLogger(ClienteDAO.class.getName()).log(Level.SEVERE, null, ex);
+        } finally {
+            if (conn != null) {
+                conn.fechar();
+            }
+        }
+        return c;
     }
     
 }
