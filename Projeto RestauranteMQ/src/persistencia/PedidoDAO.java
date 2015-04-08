34cbbf82a1b5974fb7dd0b6e13e79b3c80b862bf
RaelCappra <rael.cappra@gmail.com>
2015-04-08 09:28:28 -0300

    comments

--- Projeto RestauranteMQ/src/persistencia/PedidoDAO.java
+++ Projeto RestauranteMQ/src/persistencia/PedidoDAO.java
@@ -20,6 +20,11 @@ import model.Pedido;
  */
 public class PedidoDAO implements Dao<Pedido, Long>{
 
+    /*
+    TODO: !!!IMPORTANTAÇO!!! incluir os itens do pedido na tabela intermediaria
+    e busca-los nos metodos de query
+    */
+    
     @Override
     public void save(Pedido entity) {
         
